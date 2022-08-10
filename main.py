3537031712f0ca939ae7eda7aad0b0e460538ce2
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2022-08-10 21:02:49 +0200

    feat: add journal which quotes the biggest amount of drugs

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ This file contains the main function.
 Launch the program with the following command: `python main.py`
 """
 
+import pandas as pd
+
 from etl.etl import ETL
 
 def main() -> bool:
@@ -11,7 +13,27 @@ def main() -> bool:
     """
     return ETL().run()
 
+def get_journal_which_quotes_the_most_amount_of_drugs(data: pd.DataFrame) -> tuple[str, int]:
+    """
+    Get the journal which quotes the most amount of drugs.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        The data.json file into a pandas DataFrame.
+
+    Returns
+    -------
+    tuple[str, int]
+        The journal name and the amount of drugs.
+    """
+    ranking = data.groupby("journal").count()["drug"].sort_values(ascending=False)
+    return ranking.index[0], ranking.values[0]
+
 if __name__ == '__main__':
     if main():
         print('ETL completed successfully.')
+
+    most_prolific_journal, nb_of_drugs = get_journal_which_quotes_the_most_amount_of_drugs(pd.read_json("data.json"))
+    print('The journal which quotes the most amount of drugs is "{}" with {} different drugs.'.format(most_prolific_journal, nb_of_drugs))
     
